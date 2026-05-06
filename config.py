b0959f97c246ea9cf47895b324ffa34492c97a20
likhitha-142005 <reddylikhitha276@gmail.com>
2026-05-06 13:47:58 +0530

    config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ class Config:
     SECRET_KEY = "supersecretkey"
 
     DB_HOST = "localhost"
-    DB_USER = "root"
-    DB_PASSWORD = "96766"
-    DB_NAME = "fraud_analyser"
+    DB_USER = "user"
+    DB_PASSWORD = "password"
+    DB_NAME = "fraud"
 
     DATA_PATH = "data/transactions.csv"
     SCORED_PATH = "data/scored_transactions.csv"
